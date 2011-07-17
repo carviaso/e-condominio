0a36cc74fdf557785c8c2b60d147ec0604952327
giubueno@gmail.com <giubueno@gmail.com@1d32733a-9a7c-63fa-3abf-4dd6d3c8a6cc>
2011-07-17 22:49:59 +0000

    - Primeiro commit.

---
+++ trunk/Verano/Controllers/AccountController.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using Verano.Models;
+
+namespace Verano.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        //
+        // GET: /Account/LogOn
+
+        public ActionResult LogOn()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/LogOn
+
+        [HttpPost]
+        public ActionResult LogOn(LogOnModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Membership.ValidateUser(model.UserName, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+                    if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                        && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Nome de usuário ou senha incorreta.");
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+        //
+        // GET: /Account/LogOff
+
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        //
+        // GET: /Account/Register
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/Register
+
+        [HttpPost]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Attempt to register the user
+                MembershipCreateStatus createStatus;
+                Membership.CreateUser(model.UserName, model.Password, model.Email, null, null, true, null, out createStatus);
+
+                if (createStatus == MembershipCreateStatus.Success)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false /* createPersistentCookie */);
+                    return RedirectToAction("Index", "Morador");
+                }
+                else
+                {
+                    ModelState.AddModelError("", ErrorCodeToString(createStatus));
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+        //
+        // GET: /Account/ChangePassword
+
+        [Authorize]
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/ChangePassword
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult ChangePassword(ChangePasswordModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                // ChangePassword will throw an exception rather
+                // than return false in certain failure scenarios.
+                bool changePasswordSucceeded;
+                try
+                {
+                    MembershipUser currentUser = Membership.GetUser(User.Identity.Name, true /* userIsOnline */);
+                    changePasswordSucceeded = currentUser.ChangePassword(model.OldPassword, model.NewPassword);
+                }
+                catch (Exception)
+                {
+                    changePasswordSucceeded = false;
+                }
+
+                if (changePasswordSucceeded)
+                {
+                    return RedirectToAction("ChangePasswordSuccess");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Senha atual errada ou senha atual inválida.");
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+        //
+        // GET: /Account/ChangePasswordSuccess
+
+        public ActionResult ChangePasswordSuccess()
+        {
+            return View();
+        }
+
+        #region Status Codes
+        private static string ErrorCodeToString(MembershipCreateStatus createStatus)
+        {
+            // See http://go.microsoft.com/fwlink/?LinkID=177550 for
+            // a full list of status codes.
+            switch (createStatus)
+            {
+                case MembershipCreateStatus.DuplicateUserName:
+                    return "Nome de usuário já está sendo usado. Por favor, informe um nome de usuário diferente.";
+
+                case MembershipCreateStatus.DuplicateEmail:
+                    return "E-mail já registrado. Utilize outro e-mail.";
+
+                case MembershipCreateStatus.InvalidPassword:
+                    return "Senha inválida. Por favor, informe uma senha válida.";
+
+                case MembershipCreateStatus.InvalidEmail:
+                    return "E-mail inválido. Por favor, verifique o e-mail informado.";
+
+                case MembershipCreateStatus.InvalidAnswer:
+                    return "A senha informada é inválida. Por favor, verifique o e-mail informado.";
+
+                case MembershipCreateStatus.InvalidQuestion:
+                    return "Pergunta inválida. Por favor, verifique a pergunta informada.";
+
+                case MembershipCreateStatus.InvalidUserName:
+                    return "O nome de usuário é inválido. Por favor, verifique o valor informado e tente novamente.";
+
+                case MembershipCreateStatus.ProviderError:
+                    return "Erro interno. Por favor, verifique o valor informado e tente novamente. Se o problema persistir, Por favor, entre em contato conosco.";
+
+                case MembershipCreateStatus.UserRejected:
+                    return "O processo de ciação foi cancelado. Por favor, verifique o valor informado. Se o problema persistir, por favor entre em contato conosco.";
+
+                default:
+                    return "Erro desconhecido. Por favor, verifique o valor informado. Se o problema persistir, por favor entre em contato conosco.";
+            }
+        }
+        #endregion
+    }
+}
---
+++ trunk/Verano/Controllers/PerguntaController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Verano.Models;
+using Verano.Models.Forms;
+
+namespace Verano.Controllers
+{ 
+    public class PerguntaController : Controller
+    {
+        private VeranoEntities db = new VeranoEntities();
+
+        //
+        // GET: /Pergunta/
+
+        [Authorize(Roles="Administrador")]
+        public ViewResult Index()
+        {
+            return View(db.Pergunta.ToList());
+        }
+
+        [Authorize]
+        public ActionResult Respostas(int id)
+        {
+            ViewData["IdMorador"] = id;
+
+            var morador = db.Morador.FirstOrDefault(m => m.Id == id);
+            var respostas = db.Resposta.Where(r => r.IdMorador == id);
+
+            if (!respostas.Any())
+            {
+                return RedirectToAction("Responder", new { @id = id});
+            }
+
+            ViewData["Perguntas"] = db.Pergunta.Include("Opcao");
+            return View(respostas.ToList());
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ViewResult Responder(int id)
+        {
+            ViewData["IdMorador"] = id;
+            return View(db.Pergunta.FirstOrDefault());
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Responder(Resposta resposta)
+        {
+            ViewData["IdMorador"] = resposta.IdMorador;
+
+            Pergunta atual = db.Pergunta.FirstOrDefault(p => p.Id == resposta.IdPergunta);
+
+            Resposta existe = atual.Resposta.FirstOrDefault(r => r.IdMorador == resposta.IdMorador);
+            if (existe != null)
+            {
+                existe.IdOpcao = resposta.IdOpcao;
+                existe.Observacao = resposta.Observacao;
+                db.SaveChanges();
+                return RedirectToAction("Respostas", "Pergunta", new { @id = resposta.IdMorador});
+            }
+            else
+            {
+                db.Resposta.AddObject(resposta);
+                db.SaveChanges();
+            }
+
+            Pergunta proxima = db.Pergunta.FirstOrDefault(p => p.Id == resposta.IdPergunta + 1);
+
+            if (proxima != null)
+            {
+                return View(proxima);
+            }
+            else
+            {
+                return View("Final");
+            }
+        }
+
+        //
+        // GET: /Pergunta/Details/5
+
+        [Authorize(Roles = "Administradores")]
+        public ViewResult Details(int id)
+        {
+            Pergunta pergunta = db.Pergunta.Single(p => p.Id == id);
+            return View(pergunta);
+        }
+
+        //
+        // GET: /Pergunta/Create
+
+        [Authorize(Roles = "Administradores")]
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Pergunta/Create
+
+        [Authorize(Roles = "Administradores")]
+        [HttpPost]
+        public ActionResult Create(Pergunta pergunta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pergunta.AddObject(pergunta);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(pergunta);
+        }
+        
+        //
+        // GET: /Pergunta/Edit/5
+
+        [Authorize(Roles = "Administradores")]
+        public ActionResult Edit(int id)
+        {
+            Pergunta pergunta = db.Pergunta.Single(p => p.Id == id);
+            return View(pergunta);
+        }
+
+        //
+        // POST: /Pergunta/Edit/5
+
+        [Authorize(Roles = "Administradores")]
+        [HttpPost]
+        public ActionResult Edit(Pergunta pergunta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pergunta.Attach(pergunta);
+                db.ObjectStateManager.ChangeObjectState(pergunta, EntityState.Modified);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pergunta);
+        }
+
+        //
+        // GET: /Pergunta/Delete/5
+
+        [Authorize(Roles = "Administradores")]
+        public ActionResult Delete(int id)
+        {
+            Pergunta pergunta = db.Pergunta.Single(p => p.Id == id);
+            return View(pergunta);
+        }
+
+        //
+        // POST: /Pergunta/Delete/5
+
+        [Authorize(Roles = "Administradores")]
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Pergunta pergunta = db.Pergunta.Single(p => p.Id == id);
+            db.Pergunta.DeleteObject(pergunta);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
